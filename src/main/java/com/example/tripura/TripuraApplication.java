0b0d007af38f85c84defbaf7d7e21167fdbda3bd
rohinibobba <bobbarohini401@gmail.com>
2019-06-22 11:34:35 +0530

    first commit

---
+++ src/main/java/com/example/tripura/TripuraApplication.java
@@ -0,0 +1,13 @@
+package com.example.tripura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripuraApplication.class, args);
+	}
+
+}
